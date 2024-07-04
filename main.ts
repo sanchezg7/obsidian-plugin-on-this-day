bc1cca1a636462cda7bebf93caf20dd651ce6b82
ayogsynergy <sanchezg.eng@gmail.com>
2024-07-04 16:44:06 -0400

    Polishing prior to expanding search functionality

--- main.ts
+++ main.ts
@@ -47,9 +47,9 @@ export default class MyPlugin extends Plugin {
 		});
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+		const ribbonIconEl = this.addRibbonIcon('dice', 'View notes in the past', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
-			new OnThisDaySuggestions(this.app).open();
+			new NoteEntriesSuggestionsModal(this.app).open();
 		});
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
@@ -57,16 +57,6 @@ export default class MyPlugin extends Plugin {
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
 		statusBarItemEl.setText('Did you read notes on this day, already?');
-
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -104,7 +94,13 @@ interface SlimFile {
 }
 
 
-export class OnThisDaySuggestions extends FuzzySuggestModal<SlimFile> {
+/**
+ * Will show entries based on config
+ */
+export class NoteEntriesSuggestionsModal extends FuzzySuggestModal<SlimFile> {
+	// constructor(app: App) {
+	// 	super(app);
+	// }
 	// Returns all available suggestions.
 	getItems(): SlimFile[] {
 		const fmp = this.app.vault.fileMap;
