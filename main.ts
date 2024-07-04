50649bb9076585614a75ad91591e51bd76799655
ayogsynergy <sanchezg.eng@gmail.com>
2024-07-04 17:23:54 -0400

    Monthly strat working. Go back to fix yearly

--- main.ts
+++ main.ts
@@ -11,7 +11,55 @@ interface MyPluginSettings {
 const DEFAULT_SETTINGS: MyPluginSettings = {
 	mySetting: 'default'
 }
-const getFilesOnThisDay = (fileMap: {}) => {
+
+const getFiles = (fileMap: {}, strategy: NOTE_QUERY_STRAT) => {
+	switch (strategy) {
+		case NOTE_QUERY_STRAT.DAILY:
+			return getFilesOnThisDayMonthStrategy(fileMap);
+		case NOTE_QUERY_STRAT.MONTHLY:
+			return getFilesOnThisDayMonthStrategy(fileMap);
+		case NOTE_QUERY_STRAT.YEARLY:
+			return getFilesOnThisDayYearStrategy(fileMap);
+	}
+}
+
+interface cleanDate {
+	month: string;
+	day: string;
+}
+
+const getCleanDate = (date: Date): cleanDate => {
+	const prepadmonth = date.getMonth() + 1;
+	const month = prepadmonth < 10 ? "0" + prepadmonth : `${prepadmonth}`;
+	const prepadDay = date.getDate();
+	const day = prepadDay < 10 ? "0" + prepadDay : `${prepadDay}`;
+	return {
+		month,
+		day
+	}
+};
+
+const getFilesOnThisDayWeekStrategy = (fileMap: {}) => {
+	return Object.keys(fileMap).filter((key) => {
+		const now = new Date();
+		const prepadmonth = now.getMonth() + 1;
+		const month = prepadmonth < 10 ? "0" + prepadmonth : prepadmonth;
+		const day = now.getDate();
+		const regex = new RegExp("20\\d\\d" + month + day + "\.md");
+		return regex.test(key);
+	});
+}
+
+const getFilesOnThisDayMonthStrategy = (fileMap: {}) => {
+	return Object.keys(fileMap).filter((key) => {
+		const now = new Date();
+		const cleanDate = getCleanDate(now);
+		const regex = new RegExp("2024" + "\\d\\d" + cleanDate.day + "\.md");
+		return regex.test(key);
+	});
+}
+
+const getFilesOnThisDayYearStrategy = (fileMap: {}) => {
 	return Object.keys(fileMap).filter((key) => {
 		const now = new Date();
 		const prepadmonth = now.getMonth() + 1;
@@ -29,17 +77,27 @@ export default class MyPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('waves', 'View notes in the past', (evt: MouseEvent) => {
+		// This one can be a book? Or the tally
+		const ribbonYearly = this.addRibbonIcon('newspaper', 'View notes on this day from past years', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
-			new NoteEntriesSuggestionsModal(this.app).open();
+			new NoteEntriesSuggestionsModal(this.app, NOTE_QUERY_STRAT.YEARLY).open();
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+
+		// This one can be a calendar
+		const ribbonMonthly = this.addRibbonIcon('waves', 'View notes on this day from past months', (evt: MouseEvent) => {
+			// Called when the user clicks the icon.
+			new NoteEntriesSuggestionsModal(this.app, NOTE_QUERY_STRAT.MONTHLY).open();
+		});
+
+		// I think this one should be the newspaper because it is the most recent
+		const ribbonPastWeek = this.addRibbonIcon('tally-5', 'View notes from this past week', (evt: MouseEvent) => {
+			// Called when the user clicks the icon.
+			new NoteEntriesSuggestionsModal(this.app, NOTE_QUERY_STRAT.DAILY).open();
+		});
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Did you read notes on this day, already?');
+		statusBarItemEl.setText('Did you reflect on your notes, today?');
 	}
 
 	onunload() {
@@ -61,17 +119,25 @@ interface SlimFile {
 }
 
 
+enum NOTE_QUERY_STRAT {
+	YEARLY,
+	MONTHLY,
+	DAILY
+}
+
 /**
  * Will show entries based on config
  */
 export class NoteEntriesSuggestionsModal extends FuzzySuggestModal<SlimFile> {
-	// constructor(app: App) {
-	// 	super(app);
-	// }
+	private noteQueryStrat: NOTE_QUERY_STRAT;
+	constructor(app: App, strat: NOTE_QUERY_STRAT = NOTE_QUERY_STRAT.YEARLY) {
+		super(app);
+		this.noteQueryStrat = strat;
+	}
 	// Returns all available suggestions.
 	getItems(): SlimFile[] {
 		const fmp = this.app.vault.fileMap;
-		return getFilesOnThisDay(fmp)
+		return getFiles(fmp, this.noteQueryStrat)
 			.map(key => fmp[key])
 	}
 
